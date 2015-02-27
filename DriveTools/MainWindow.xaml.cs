25ccd7e1b36185fec5825b831cf33436542d26c0
Ben Arthur <kc0isg@gmail.com>
2015-02-27 14:27:41 -0700

    Created basic menu. Began building pageable generic list view.

--- DriveTools/MainWindow.xaml.cs
+++ DriveTools/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Configuration;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Forms;
 
 namespace DriveTools
 {
@@ -20,9 +11,59 @@ namespace DriveTools
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static byte[] Certificate { get; set; }
+        public static CertificateType SelectedCertificateType { get; set; }
+
+        public enum CertificateType
+        {
+            P12,
+            JSON,
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Close_OnClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void OpenJSON_OnClick(object sender, RoutedEventArgs e)
+        {
+            SelectedCertificateType = CertificateType.JSON;
+            OpenCert("JSON (*.json)|*.json");
+        }
+
+        private void OpenP12_OnClick(object sender, RoutedEventArgs e)
+        {
+            SelectedCertificateType = CertificateType.P12;
+            OpenCert("P12 (*.p12)|*.p12");
+        }
+
+        private static void OpenCert(string fileExtensionFilter)
+        {
+            var stream = new MemoryStream();
+            var openFileDialog = new OpenFileDialog
+            {
+                InitialDirectory = @"C:\",
+                Filter = fileExtensionFilter + @"|All Files (*.*)|*.*",
+                FilterIndex = 1,
+                RestoreDirectory = true
+            };
+
+            if (openFileDialog.ShowDialog() != System.Windows.Forms.DialogResult.OK) return;
+            try
+            {
+                openFileDialog.OpenFile().CopyTo(stream);
+                Certificate = stream.ToArray();
+            }
+            catch (Exception ex)
+            {
+                    
+                throw;
+            }
         }
     }
 }
